7fcd36c107b5f8e6439dc91fb4326e20cee54b6a
Jacob H <99051071+Jacob-Ham@users.noreply.github.com>
2024-01-16 21:20:43 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,5 +15,5 @@ cd /tmp && git clone https://github.com/Jacob-Ham/LOLDoors.git && cd LOLDoors &&
 cd /tmp && wget https://mega.nz/linux/repo/Debian_12/amd64/megasync-Debian_12_amd64.deb && sudo apt install ./megasync-Debian_12_amd64.deb -y
 # Install espanso
 mkdir -p ~/opt && wget -O ~/opt/Espanso.AppImage 'https://github.com/federico-terzi/espanso/releases/download/v2.1.8/Espanso-X11.AppImage' && chmod u+x ~/opt/Espanso.AppImage && sudo ~/opt/Espanso.AppImage env-path register && espanso service register
-rm ~/.config/espanso/match/base.yml && wget https://raw.githubusercontent.com/Jacob-Ham/kali-configs/main/espanso/base.yml -P ~/.config/espanso/match/
+rm ~/.config/espanso/match/base.yml && wget https://raw.githubusercontent.com/Jacob-Ham/kali-configs/main/espanso/base.yml -P ~/.config/espanso/match/ && espanso start
 rm ~/.config/terminator/config && wget https://raw.githubusercontent.com/Jacob-Ham/kali-configs/main/terminator/config -P ~/.config/terminator/
