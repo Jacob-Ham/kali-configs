7551fe8f3226391e4bcb362116500107a85c1689
Jacob H <99051071+Jacob-Ham@users.noreply.github.com>
2024-03-17 09:49:26 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,9 +11,10 @@ sudo apt install remmina -y
 cd /tmp && git clone https://github.com/Jacob-Ham/gbins.git && cd gbins && pip3 install -r requirements.txt && sudo /usr/bin/chmod u+x gbins.py && /usr/bin/sudo /usr/bin/cp -p gbins.py /usr/bin/gbins
 # install LOLDoors
 cd /tmp && git clone https://github.com/Jacob-Ham/LOLDoors.git && cd LOLDoors && sudo /usr/bin/chmod u+x LOLDoors.py && /usr/bin/sudo /usr/bin/cp -p LOLDoors.py /usr/bin/LOLDoors
-# Install megasync
-cd /tmp && wget https://mega.nz/linux/repo/Debian_12/amd64/megasync-Debian_12_amd64.deb && sudo apt install ./megasync-Debian_12_amd64.deb -y
+# Go to pcloud download
+firfox 'https://www.pcloud.com/how-to-install-pcloud-drive-linux.html?download=electron-64'
 # Install espanso
 mkdir -p ~/opt && wget -O ~/opt/Espanso.AppImage 'https://github.com/federico-terzi/espanso/releases/download/v2.1.8/Espanso-X11.AppImage' && chmod u+x ~/opt/Espanso.AppImage && sudo ~/opt/Espanso.AppImage env-path register && espanso service register
 rm ~/.config/espanso/match/base.yml && wget https://raw.githubusercontent.com/Jacob-Ham/kali-configs/main/espanso/base.yml -P ~/.config/espanso/match/ && espanso start
 rm ~/.config/terminator/config && wget https://raw.githubusercontent.com/Jacob-Ham/kali-configs/main/terminator/config -P ~/.config/terminator/
+
