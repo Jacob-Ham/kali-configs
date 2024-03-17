13078d126b9081bc1018a0525fd8fd6be754eca9
Jacob H <99051071+Jacob-Ham@users.noreply.github.com>
2024-03-17 09:54:34 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,6 +13,10 @@ cd /tmp && git clone https://github.com/Jacob-Ham/gbins.git && cd gbins && pip3 
 cd /tmp && git clone https://github.com/Jacob-Ham/LOLDoors.git && cd LOLDoors && sudo /usr/bin/chmod u+x LOLDoors.py && /usr/bin/sudo /usr/bin/cp -p LOLDoors.py /usr/bin/LOLDoors
 # Go to pcloud download
 firfox 'https://www.pcloud.com/how-to-install-pcloud-drive-linux.html?download=electron-64'
+# Install netexec
+sudo apt install pipx git
+pipx ensurepath
+pipx install git+https://github.com/Pennyw0rth/NetExec
 # Install espanso
 mkdir -p ~/opt && wget -O ~/opt/Espanso.AppImage 'https://github.com/federico-terzi/espanso/releases/download/v2.1.8/Espanso-X11.AppImage' && chmod u+x ~/opt/Espanso.AppImage && sudo ~/opt/Espanso.AppImage env-path register && espanso service register
 rm ~/.config/espanso/match/base.yml && wget https://raw.githubusercontent.com/Jacob-Ham/kali-configs/main/espanso/base.yml -P ~/.config/espanso/match/ && espanso start
