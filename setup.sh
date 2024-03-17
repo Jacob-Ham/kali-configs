645f6940ae5e9199a33dd9dbd040fcf56318e162
Jacob H <99051071+Jacob-Ham@users.noreply.github.com>
2024-03-17 09:56:29 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,6 +17,12 @@ firfox 'https://www.pcloud.com/how-to-install-pcloud-drive-linux.html?download=e
 sudo apt install pipx git
 pipx ensurepath
 pipx install git+https://github.com/Pennyw0rth/NetExec
+# netexec tab completion
+sudo apt install python3-argcomplete
+# For Bash
+register-python-argcomplete nxc >> ~/.bashrc
+# For Zsh
+register-python-argcomplete nxc >> ~/.zshrc
 # Install espanso
 mkdir -p ~/opt && wget -O ~/opt/Espanso.AppImage 'https://github.com/federico-terzi/espanso/releases/download/v2.1.8/Espanso-X11.AppImage' && chmod u+x ~/opt/Espanso.AppImage && sudo ~/opt/Espanso.AppImage env-path register && espanso service register
 rm ~/.config/espanso/match/base.yml && wget https://raw.githubusercontent.com/Jacob-Ham/kali-configs/main/espanso/base.yml -P ~/.config/espanso/match/ && espanso start
